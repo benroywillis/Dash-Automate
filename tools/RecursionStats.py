7d22ceb9fdbfb47c085b4040fe88aa4a9db72527
benroywillis <benroywillis@gmail.com>
2022-08-24 08:10:43 -0700

    Improved function stats figure

--- tools/RecursionStats.py
+++ tools/RecursionStats.py
@@ -14,14 +14,15 @@ axisFont  = 10
 axisLabelFont  = 10
 titleFont = 16
 xtickRotation = 90
-colors = [ ( 50./255 , 162./255, 81./255 , 255./255 ), # leaf green
-           ( 255./255, 127./255, 15./255 , 255./255 ), # crimson red
-       	   ( 214./255, 39./255 , 40./255 , 255./255 ), # orange
-           ( 121./255, 154./255, 134./255, 255./255 ), # olive green
-           ( 190./255, 10./255 , 255./255, 255./255 ), # violet
-           ( 180./255, 90./255 , 0.0     , 255./255 ), # brown
-           ( 255./255, 10./255 , 140./255, 255./255 ), # hot pink
-           ( 198./255, 195./255, 71./255 , 255./255 ) ]# mustard yellow
+colors = [ ( 50./255 , 162./255, 81./255 , 127./255 ), # leaf green
+           ( 255./255, 127./255, 15./255 , 127./255 ), # crimson red
+       	   ( 214./255, 39./255 , 40./255 , 127./255 ), # orange
+           ( 121./255, 154./255, 134./255, 127./255 ), # olive green
+           ( 190./255, 10./255 , 255./255, 127./255 ), # violet
+           ( 180./255, 90./255 , 0.0     , 127./255 ), # brown
+           ( 255./255, 10./255 , 140./255, 127./255 ), # hot pink
+           ( 198./255, 195./255, 71./255 , 127./255 ), # mustard yellow
+           ( 204./255, 153./255, 255./255, 127./255 ) ]# light violet 
 markers = [ 'o', '^', '1', 's', '*', 'd', 'X', '>']
 
 def recursiveFunctionRegex(line):
@@ -29,8 +30,9 @@ def recursiveFunctionRegex(line):
 	totalLiveF = re.findall("\sTOTAL\sLIVE\sFUNCTIONS:\s\d+\s", line)
 	IDRF       = re.findall("\sINDIRECT\sRECURSION\sFUNCTIONS:\s\d+\s", line)
 	DRF        = re.findall("\sDIRECT\sRECURSION\sFUNCTIONS:\s\d+\s", line)
-	if (len(totalF) + len(totalLiveF) + len(IDRF) + len(DRF)) > 1:
-		raise Exception("Got many results from a single recursive function regex! TotalF:" +str(totalF)+", totalLiveF:"+str(totalLiveF)+", IDRF: "+str(IDRF)+", DRF: "+str(DRF))
+	TFP        = re.findall("\sTOTAL\sFUNCTION\sPOINTERS:\s\d+\s", line)
+	if (len(totalF) + len(totalLiveF) + len(IDRF) + len(DRF) + len(TFP)) > 1:
+		raise Exception("Got many results from a single recursive function regex! TotalF:" +str(totalF)+", totalLiveF:"+str(totalLiveF)+", IDRF: "+str(IDRF)+", DRF: "+str(DRF)+", TFP: "+str(TFP))
 	if len(totalF):
 		return totalF[0]
 	elif len(totalLiveF):
@@ -39,10 +41,12 @@ def recursiveFunctionRegex(line):
 		return IDRF[0]
 	elif len(DRF):
 		return DRF[0]
+	elif len(TFP):
+		return TFP[0]
 	else:
 		return ""
 
-def plotRecursionData(dataMap):
+def getProjectData(dataMap):
 	projectMap = {}
 	for path in dataMap:
 		if len(dataMap[path]) != 8:
@@ -50,7 +54,7 @@ def plotRecursionData(dataMap):
 			continue
 		pName = RD.getProjectName(path, "Dash-Corpus")
 		if projectMap.get(pName) is None:
-			projectMap[pName] = { "Static": { "Total": 0, "Live": 0, "IDR": 0, "DR": 0 }, "Dynamic": { "Total": 0, "Live": 0, "IDR": 0, "DR": 0 } }
+			projectMap[pName] = { "Static": { "Total": 0, "Live": 0, "IDR": 0, "DR": 0, "TFP": 0 }, "Dynamic": { "Total": 0, "Live": 0, "IDR": 0, "DR": 0 } }
 		# there are two entries of each category ("TOTAL FUNCTIONS", and so on) in a given dataMap[path]
 		# the first entry is the static number, the second is the dynamic number
 		sTotalFound = False
@@ -94,9 +98,14 @@ def plotRecursionData(dataMap):
 					sDRFound = True
 				else:
 					projectMap[pName]["Dynamic"]["DR"] += int(num[0])
+			elif "TOTAL FUNCTION POINTERS: " in entry:
+				num = re.findall("\d+", entry)
+				if not len(num):
+					raise Exception("Entry "+path+" did not have a number in it! - "+entry)
+				projectMap[pName]["Static"]["TFP"] += int(num[0])
 			else:
 				raise Exception("Found an entry that didn't match any key stats! - " + entry)
-	projectMap["Total"] = { "Static": { "Total": 0, "Live": 0, "IDR": 0, "DR": 0 }, "Dynamic": { "Total": 0, "Live": 0, "IDR": 0, "DR": 0 } }
+	projectMap["Total"] = { "Static": { "Total": 0, "Live": 0, "IDR": 0, "DR": 0, "TFP": 0 }, "Dynamic": { "Total": 0, "Live": 0, "IDR": 0, "DR": 0 } }
 	for entry in projectMap:
 		if entry == "Total":
 			continue
@@ -104,12 +113,14 @@ def plotRecursionData(dataMap):
 		projectMap["Total"]["Static"]["Live"]  += projectMap[entry]["Static"]["Live"]
 		projectMap["Total"]["Static"]["IDR"]   += projectMap[entry]["Static"]["IDR"]
 		projectMap["Total"]["Static"]["DR"]    += projectMap[entry]["Static"]["DR"]
+		projectMap["Total"]["Static"]["TFP"]    += projectMap[entry]["Static"]["TFP"]
 		projectMap["Total"]["Dynamic"]["Total"] += projectMap[entry]["Dynamic"]["Total"]
 		projectMap["Total"]["Dynamic"]["Live"]  += projectMap[entry]["Dynamic"]["Live"]
 		projectMap["Total"]["Dynamic"]["IDR"]   += projectMap[entry]["Dynamic"]["IDR"]
 		projectMap["Total"]["Dynamic"]["DR"]    += projectMap[entry]["Dynamic"]["DR"]
-	print(projectMap)
+	return projectMap
 
+def plotRecursionData_bars(projectMap):
 	xTickLabels = list(projectMap.keys())
 	
 	fig = plt.figure(frameon=False)
@@ -122,6 +133,7 @@ def plotRecursionData(dataMap):
 	ax.bar( [x for x in range(len(xTickLabels))], [projectMap[y]["Static"]["Live"] for y in projectMap], label="Static-Live", color=colors[2] )
 	ax.bar( [x for x in range(len(xTickLabels))], [projectMap[y]["Static"]["IDR"] for y in projectMap], label="Static-Indirect", color=colors[4] )
 	ax.bar( [x for x in range(len(xTickLabels))], [projectMap[y]["Static"]["DR"] for y in projectMap], label="Static-Direct", color=colors[6] )
+	ax.bar( [x for x in range(len(xTickLabels))], [projectMap[y]["Static"]["TFP"] for y in projectMap], label="Total Function Pointers", color=colors[8] )
 	ax.set_yscale("log")
 	ax.legend(frameon=False)
 	plt.xticks(color="white")
@@ -140,5 +152,31 @@ def plotRecursionData(dataMap):
 	RD.PrintFigure(plt, "RecursionBars")
 	plt.show()
 
+def plotRecursionData_scatter(projectMap):
+	fig = plt.figure(frameon=False)
+	fig.set_facecolor("Black")
+	ax = fig.add_subplot(1, 1, 1, frameon=False, fc="black")
+
+	xtickLabels = list(projectMap.keys())
+	ax.set_title("Static Function Overheads", fontsize=titleFont)
+
+	ax.scatter( xtickLabels, [projectMap[p]["Dynamic"]["Live"] / projectMap[p]["Static"]["Total"] * 100 for p in projectMap], label="Live", color=colors[0], marker=markers[0] )
+	ax.scatter( xtickLabels, [projectMap[p]["Static"]["TFP"] / projectMap[p]["Static"]["Total"] * 100 for p in projectMap], label="Function Pointers", color=colors[1], marker=markers[1] )
+	ax.scatter( xtickLabels, [projectMap[p]["Dynamic"]["DR"] / projectMap[p]["Dynamic"]["Live"] * 100 for p in projectMap], label="Recursive", color=colors[2], marker=markers[2] )
+	#ax.scatter( xtickLabels, [projectMap[p]["Dynamic"]["IDR"] / projectMap[p]["Dynamic"]["Live"] * 100 for p in projectMap], label="Indirect Recursive", color=colors[3], marker=markers[3] )
+	#ax.scatter( xtickLabels, [projectMap[p]["Static"]["Total"] for p in projectMap], label="Static-Total", color=colors[0] )
+
+	ax.set_xlabel("Project", fontsize=axisFont)
+	ax.set_ylabel("%", fontsize=axisFont)
+	ax.set_ylim([0, 100])
+	ax.legend(frameon=False)
+	plt.xticks(fontsize=axisFont, rotation=xtickRotation)
+	for i in range(10, 110, 10):
+		plt.axhline(i, linestyle="dashed", color="grey")
+	RD.PrintFigure(plt, "RecursionScatter")
+	plt.show()
+	
 dataMap = RD.retrieveLogData(RD.buildFolders, RD.CorpusFolder, dataFileName, recursiveFunctionRegex)
-plotRecursionData(dataMap)
+projectMap = getProjectData(dataMap)
+#plotRecursionData_bars(projectMap)
+plotRecursionData_scatter(projectMap)
