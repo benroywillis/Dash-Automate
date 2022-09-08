fa3fa467a24e8b6b2aec17a547032fbfb054f8ea
benroywillis <benroywillis@gmail.com>
2022-09-08 12:12:34 -0700

    Added compliance for memory pass and changed the names of the figures to specialize for the build folder their data comes from

--- tools/Compliance.py
+++ tools/Compliance.py
@@ -3,7 +3,9 @@ import re
 import json
 import matplotlib.pyplot as plt
 
-dataFileName = "Compliance_"+"".join(x for x in RD.CorpusFolder.split("/"))+list(RD.buildFolders)[0]+".json"
+cartographerFileName    = "Compliance_Cartographer_"+"".join(x for x in RD.CorpusFolder.split("/"))+list(RD.buildFolders)[0]+".json"
+memoryPassFileName      = "Compliance_MemoryPass_"+"".join(x for x in RD.CorpusFolder.split("/"))+list(RD.buildFolders)[0]+".json"
+kernelGrammarFileName   = "Compliance_KernelGrammar_"+"".join(x for x in RD.CorpusFolder.split("/"))+list(RD.buildFolders)[0]+".json"
 
 # plot parameters
 axisFont  = 10
@@ -43,6 +45,25 @@ def readCartographerLog(line):
 		return line.split(":")[-1]
 	return ""
 
+def readMemoryPassLog(line):
+	success = re.findall("DAStepSuccess\:\sMemoryRun\scommand\ssucceeded", line)
+	if len(success):
+		# this means the program went from start to finish without a fatal error
+		return "success"
+	errors = re.findall("DAStepError\:\s\w+\scommand\sfailed", line)
+	if len(errors):
+		return "errors"
+	exceptions = re.findall("what\(\)\:\s+std\:\:exception", line)
+	if len(exceptions):
+		return "unhandled exception"
+	interrupt = re.findall("Script\sinterrupted\!\sDestroying\stmp\sfolder\.", line)
+	if len(interrupt):
+		return "interrupt"
+	reasons = re.findall("\[critical\]", line)
+	if len(reasons):
+		return line.split(":")[-1]
+	return ""
+
 # categories are:
 # 1. Compliant: no errors detected
 # 2. Inlining Schedule: could not insert a parent after all its inlinable dependencies were scheduled
@@ -52,7 +73,7 @@ def readCartographerLog(line):
 # 6. Function Subgraph: could not correctly find a function subgraph
 # 7. Transform Error: problem with a transform, could be inline transform or cfg transform
 # 8. Unknown: unhandled exception
-def mapErrorMessage(error):
+def mapCartographerErrorMessage(error):
 	if error.startswith("success"):
 		return "Compliant"
 	elif error.startswith(" Inlinable embedded function edge"):
@@ -82,7 +103,55 @@ def mapErrorMessage(error):
 	else:
 		return "Unknown"
 
-def plotCompliance(results):
+def mapMemoryPassErrorMessage(error):
+	if error.startswith("success"):
+		return "Compliant"
+	elif error.startswith("Kernel entrance sink node intersected"):
+		return "Kernel boundary"
+	elif error.startswith("Kernel exit source node intersected"):
+		return "Kernel boundary"
+	elif error.startswith("In Progress"):
+		return "In Progress"
+	else:
+		return "Unknown"
+
+def sortAutomationLogData(dataMap, type="Cartographer"):
+	"""
+	"""
+	results = {}
+	for entry in sorted(dataMap):
+		projectName = RD.getProjectName(entry, "Dash-Corpus")
+		if results.get(projectName) is None:
+			results[projectName] = { "success": 0 }
+		if len(dataMap[entry]) == 0:
+			dataMap[entry] = ["In Progress"]
+		if dataMap[entry][0] == "success":
+			results[projectName]["success"] += 1
+		else:
+			if results[projectName].get(dataMap[entry][0]) is None:
+				results[projectName][dataMap[entry][0]] = 1
+			else:
+				results[projectName][dataMap[entry][0]] += 1
+
+	results["Total"] = { "success": 0 }
+	for entry in results:
+		if entry != "Total":
+			for category in results[entry]:
+				if results["Total"].get(category) is None:
+					results["Total"][category] = results[entry][category]
+				else:
+					results["Total"][category] += results[entry][category]
+	results["Ignore"] = {}
+	results["Ignore"]["Total"] = sum([results["Total"][x] for x in results["Total"]])
+	results["Ignore"]["Errors"] = results["Ignore"]["Total"] - results["Total"]["success"] - results["Total"].get("In Progress", 0)
+	results["Ignore"]["In Progress"] = results["Total"].get("In Progress", 0)
+	results["Ignore"]["Compliance"] = results["Total"]["success"] / ( results["Ignore"]["Total"] - results["Ignore"].get("In Progress", 0) )
+	with open("Data/Results_Compliance_"+type+"_"+"".join(x for x in RD.CorpusFolder.split("/"))+list(RD.buildFolders)[0]+".json", "w") as f:
+		json.dump(results, f, indent=4)
+
+	return results
+
+def plotCartographerCompliance(results):
 	fig = plt.figure(frameon=False)
 	fig.set_facecolor("black")
 	ax = fig.add_subplot(1, 1, 1, frameon=False, fc="black")
@@ -98,7 +167,7 @@ def plotCompliance(results):
 		if p != "Ignore":
 			mappedResults[p] = { "Compliant": 0, "Inlining Schedule": 0, "Static Information Injection": 0, "Evaluation Time Too Long": 0, "Call-Return Mapping": 0, "Profile Read Error": 0, "Function Subgraph": 0, "Transform Error": 0, "In Progress": 0, "Unknown": 0 }
 			for e in results[p]:
-				mappedResults[p][mapErrorMessage(e)] += results[p][e]
+				mappedResults[p][mapCartographerErrorMessage(e)] += results[p][e]
 	compliant      = [mappedResults[p]["Compliant"] for p in mappedResults]
 	inlineSchedule = [mappedResults[p]["Inlining Schedule"] for p in mappedResults]
 	sii            = [mappedResults[p]["Static Information Injection"] for p in mappedResults]
@@ -121,42 +190,60 @@ def plotCompliance(results):
 	ax.bar([x for x in range(len(sii))], sii, bottom=[compliant[i]+inlineSchedule[i]+ettl[i]+callReturnMap[i]+profile[i]+subgraph[i]+transform[i] for i in range(len(compliant))], label="Static Information Injection", color=colors[7])
 	ax.bar([x for x in range(len(unknown))], inProgress, bottom=[compliant[i]+inlineSchedule[i]+ettl[i]+callReturnMap[i]+profile[i]+subgraph[i]+transform[i]+sii[i] for i in range(len(compliant))], label="InProgress", color=colors[8])
 	ax.bar([x for x in range(len(unknown))], unknown, bottom=[compliant[i]+inlineSchedule[i]+ettl[i]+callReturnMap[i]+profile[i]+subgraph[i]+transform[i]+sii[i]+inProgress[i] for i in range(len(compliant))], label="Unknown", color=colors[9])
+	ax.set_title("Cartographer Compliance")
 	ax.set_ylabel("Count", fontsize=axisLabelFont)
 	ax.set_xlabel("Application", fontsize=axisLabelFont)
 	plt.xticks(ticks=[x for x in range( len(xtickLabels) )], labels=xtickLabels, fontsize=axisFont, rotation=xtickRotation)
 	ax.legend(frameon=False)
-	RD.PrintFigure(plt, "Compliance")
+	RD.PrintFigure(plt, "Compliance_Cartographer")
 	plt.show()
 
-dataMap = RD.retrieveLogData(RD.buildFolders, RD.CorpusFolder, dataFileName, readCartographerLog)
-results = {}
-for entry in sorted(dataMap):
-	projectName = RD.getProjectName(entry, "Dash-Corpus")
-	if results.get(projectName) is None:
-		results[projectName] = { "success": 0 }
-	if len(dataMap[entry]) == 0:
-		dataMap[entry] = ["In Progress"]
-	if dataMap[entry][0] == "success":
-		results[projectName]["success"] += 1
-	else:
-		if results[projectName].get(dataMap[entry][0]) is None:
-			results[projectName][dataMap[entry][0]] = 1
-		else:
-			results[projectName][dataMap[entry][0]] += 1
+def plotMemoryPassCompliance(results):
+	"""
+	"""
+	fig = plt.figure(frameon=False)
+	fig.set_facecolor("black")
+	ax = fig.add_subplot(1, 1, 1, frameon=False, fc="black")
 
-results["Total"] = { "success": 0 }
-for entry in results:
-	if entry != "Total":
-		for category in results[entry]:
-			if results["Total"].get(category) is None:
-				results["Total"][category] = results[entry][category]
-			else:
-				results["Total"][category] += results[entry][category]
-results["Ignore"] = {}
-results["Ignore"]["Total"] = sum([results["Total"][x] for x in results["Total"]])
-results["Ignore"]["Errors"] = results["Ignore"]["Total"] - results["Total"]["success"] - results["Total"]["In Progress"]
-results["Ignore"]["In Progress"] = results["Total"]["In Progress"]
-results["Ignore"]["Compliance"] = results["Total"]["success"] / ( results["Ignore"]["Total"] - results["Ignore"]["In Progress"] )
-with open("Data/Results_Compliance_"+"".join(x for x in RD.CorpusFolder.split("/"))+list(RD.buildFolders)[0]+".json", "w") as f:
-	json.dump(results, f, indent=4)
-plotCompliance(results)
+	# x axis labels
+	xtickLabels = []
+	for p in results:
+		if p != "Ignore":
+			xtickLabels.append(p)
+
+	mappedResults = {}
+	categories = { "Compliant": 0, "File Read Error": 0, "Kernel boundary": 0, "In Progress": 0, "Unknown": 0 }
+	dataLabels = list(categories)
+	for p in results:
+		if p != "Ignore":
+			mappedResults[p] = { "Compliant": 0, "File Read Error": 0, "Kernel boundary": 0, "In Progress": 0, "Unknown": 0 }
+			for e in results[p]:
+				mappedResults[p][mapMemoryPassErrorMessage(e)] += results[p][e]
+
+	i = 0
+	for i in range(len(dataLabels)):
+		if i == 0:
+			ax.bar([p for p in mappedResults], [mappedResults[p][dataLabels[i]] for p in mappedResults], label=dataLabels[i], color=colors[i])
+		else:
+			barBottom = []
+			for p in mappedResults:
+				sum = 0
+				for k in range(i):
+					sum += mappedResults[p][dataLabels[k]]
+				barBottom.append( sum )
+			ax.bar([p for p in mappedResults], [mappedResults[p][dataLabels[i]] for p in mappedResults], bottom=barBottom, label=dataLabels[i], color=colors[i])
+		i += 1
+	ax.set_title("Memory Pass Compliance")
+	ax.set_ylabel("Count", fontsize=axisLabelFont)
+	ax.set_xlabel("Application", fontsize=axisLabelFont)
+	plt.xticks(ticks=[x for x in range( len(xtickLabels) )], labels=xtickLabels, fontsize=axisFont, rotation=xtickRotation)
+	ax.legend(frameon=False)
+	RD.PrintFigure(plt, "Compliance_MemoryPass")
+	plt.show()
+
+cartographerMap = RD.retrieveLogData(RD.buildFolders, RD.CorpusFolder, cartographerFileName, readCartographerLog)
+memoryPassMap   = RD.retrieveLogData(RD.buildFolders, RD.CorpusFolder, memoryPassFileName, readMemoryPassLog, Prefix="MemoryPass_")
+cartographerResults = sortAutomationLogData(cartographerMap)
+memoryPassResults   = sortAutomationLogData(memoryPassMap, type="MemoryPass")
+plotCartographerCompliance(cartographerResults)
+plotMemoryPassCompliance(memoryPassResults)
