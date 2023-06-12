62eab1f396849ddd810c18f2b3459ca8ea0dff5b
benroywillis <benroywillis@gmail.com>
2023-06-12 11:04:10 -0700

    Added exclusive region method that uses the modernized combined data files. Shows a discrepancy between its result and the venn3 library result

--- tools/BasicBlockCorrespondence.py
+++ tools/BasicBlockCorrespondence.py
@@ -96,54 +96,31 @@ def PlotKernelCorrespondence_static(dataMap, loopMap):
 	RD.PrintFigure(plt, "BasicBlockCorrespondence")
 	plt.show()
 
-def ExclusionZones(dataMap, loopMap):
-	exclusions = { 
-					# hotcode not captured by PaMul
-				   "HC": [], \
-					# hotcode and hotloop not captured by PaMul
-				   "HC,HL": [], \
-					# hotloop not captured by PaMul
-				   "HL": []
-				 }
+def ExclusionZones(dataMap):
 	# observe the unique blocks of each data file and create the sets for each segmentation method
+	exclusions = {}
 	HC = set()
 	HL = set()
-	PaMul = set()
+	Cyclebite = set()
 	Loops = set()
 	for file in dataMap:
-		if "HotCode" in file:
-			HC = HC.union( RD.Uniquify(file, dataMap[file]["Kernels"], tn=False) )
-		elif "HotLoop" in file:
-			HL = HL.union( RD.Uniquify(file, dataMap[file]["Kernels"], tn=False) )
-		else:
-			PaMul = PaMul.union( RD.Uniquify(file, dataMap[file]["Kernels"], tn=False) )
-	# find files that contribute to the exclusion zones
-	for file in dataMap:
-		uniqueBlocks = RD.Uniquify(file, dataMap[file]["Kernels"], tn=False)
-		if "HotCode" in file:
-			# overlap the blocks with the PaMul set and the hotloop set
-			intersect_p = uniqueBlocks.intersection(PaMul)
-			intersect_l = uniqueBlocks.intersection(HL)
-			onlyHC = uniqueBlocks - intersect_p - intersect_l
-			# if the overlaps are not the size of the file coverage in both cases, there are unique blocks in the hotcode result that are not in either the paMul or hotloop coverage
-			if len(onlyHC):
-				exclusions["HC"].append( (file,list(RD.reverseUniquify(onlyHC, file))) )
-			else:
-				continue
-		elif "HotLoop" in file:
-			intersect_p = uniqueBlocks.intersection(PaMul)
-			intersect_c = uniqueBlocks.intersection(HC)
-			onlyHL = uniqueBlocks - intersect_p - intersect_c
-			if len(onlyHL):
-				exclusions["HL"].append( (file,list(RD.reverseUniquify(onlyHL, file))) )
-			else:
-				continue
-		else:
-			continue
-		
-	with open("Data/ExclusiveRegions.json", "w") as f:
-		json.dump(exclusions, f, indent=4)
+		HC = HC.union( RD.Uniquify(file, dataMap[file]["hotcode"], tn=False) )
+		HL = HL.union( RD.Uniquify(file, dataMap[file]["hotloop"], tn=False) )
+		Cyclebite = Cyclebite.union( RD.Uniquify(file, dataMap[file]["pamul"], tn=False) )
+	
+		exclusions[file] = { "HC": 0, "HL": 0, "Cyclebite": 0, "HCHL": 0, "HCCyclebite": 0, "HLCyclebite": 0, "HCHLCyclebite": 0, "Total": 0 }
+		exclusions[file]["HC"] = len( HC - HL - Cyclebite )
+		exclusions[file]["HL"] = len( HL - HC - Cyclebite )
+		exclusions[file]["Cyclebite"] = len( Cyclebite - HC - HL )
+		exclusions[file]["HCHL"] = len( HC.intersection(HL) - Cyclebite )
+		exclusions[file]["HCCyclebite"] = len( HC.intersection(Cyclebite) - HL )
+		exclusions[file]["HLCyclebite"] = len( HL.intersection(Cyclebite) - HC )
+		exclusions[file]["HCHLCyclebite"] = len( HC.intersection(HL).intersection(Cyclebite) )
+		exclusions[file]["Total"] = len( HC.union(HL).union(Cyclebite) )
+
+	RD.DumpData(exclusions, "ExclusiveRegions")		
 
 combined = RD.RetrieveData(loop=True, hotcode=True, hotloop=True, pamul=True, instance=True)
 #PlotKernelCorrespondence_Manual(combined)
+ExclusionZones(combined)
 PlotKernelCorrespondence(combined)
